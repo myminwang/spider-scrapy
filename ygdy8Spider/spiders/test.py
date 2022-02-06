1fe517c803df9d0e6ea3b8b91c56da4aa4d01034
Enjoy <864443695@qq.com>
2022-02-06 14:09:31 +0800

    commit
    commit

---
+++ ygdy8Spider/spiders/test.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+content_list = [
+    '<div id="Zoom"><!--Content Start--><span style="FONT-SIZE: 12px"></span><td><img border="0" src="https://img2.doubanio.com/view/photo/l_ratio_poster/public/p2772925591.jpg" alt="" style="MAX-WIDTH: 400px">',
+    '', '◎译名爱情神话  B for Busy / Myth of Love', '◎年代2021', '◎产地中国大陆', '◎类别剧情 / 喜剧 /  爱情', '◎语言普通话 / 沪语', '◎字幕中英双字',
+    '◎上映日期2021-12-24(中国大陆)', '◎IMDb评分7.8/10  from 218 users', '◎豆瓣评分8.3/10 from 275268 users', '◎片长112分钟',
+    '◎导演邵艺辉  Yihui Shao', '◎编剧邵艺辉 Yihui Shao', '◎演员徐峥 Zheng Xu', '马伊琍 Yili  Ma', '吴越 Yue Wu', '倪虹洁 Hongjie Ni',
+    '周野芒 Yemang  Zhou', '黄明昊 Minghao Huang', '王影璐 Yinglu Wang', '宁理Li  Ning', '张芝华 Zhihua Zhang', '吴冕 Mian Wu',
+    '冯玛娅 Maya  Feng', '哈马·穆罕默德·纳吉萨拉米 Hamzah Mohamed Nagi Al-salami', '徐峥 Zheng  Xu', '陈砺志 Lizhi Chen',
+    '刘瑞芳 Ruifang Liu', "王健儿 Jian'er  Wang", '高松', '周展 Zhan Zhou', '岳洋 Yang  Yue', '梁静', '林丽君', '徐欣', '覃燕妮', '叶婷', '吴婷婷',
+    '文子  Wen Zi', '谢征宇 Zhengyu Xie', '黄琼逸 Yiqiong Huang', '傅英彰  Yingzhang Fu', '魏湘容 Hsiang-Jung Wei', '温波 Bo  Wen', '',
+    '◎简介', '',
+    '李小姐（马伊琍 饰），带着女儿过着单身生活。蓓蓓（吴越 饰），离婚后热衷跳探戈。格洛瑞亚（倪虹洁  饰），有钱有闲老公失踪。三个完全不同的女人因为老白（徐峥  饰）而产生了交集。上海爷叔老白自称杂家，专门教老年人画画。老白的好友老乌谈吐风趣，一生追逐浪漫。几顿饭局，让老白家的客厅成为了众人的聚会点。三女两男，在上海这座摩登之都里，凭借智慧、幽默过招，描摹出多样的市井烟火。',
+    '', '',
+    '<a target="_blank" href="magnet:?xt=urn:btih:4b76873047634e0a71ec612fa0cfde4afbd74d9e&amp;dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1www.ygdy8.com.%e7%88%b1%e6%83%85%e7%a5%9e%e8%af%9d.2021.HD.1080P.%e5%9b%bd%e8%af%ad%e4%b8%ad%e8%8b%b1%e5%8f%8c%e5%ad%97.mkv&amp;tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&amp;tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce"><strong><font style="BACKGROUND-COLOR: #ff9966"><font color="#0000ff"><font size="4">磁力链  爱情神话.2021.HD.1080P.国语中英双字</font></font></font></strong></a> ',
+    '<center></center></td>      </div>']
--- ygdy8Spider/spiders/ygdy8.py
+++ ygdy8Spider/spiders/ygdy8.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
 
 
 class Ygdy8Spider(scrapy.Spider):
@@ -13,5 +14,27 @@ class Ygdy8Spider(scrapy.Spider):
     def parse(self, response):
         # 测试一下
         movie_detail_urls = response.xpath('//div[@class="co_content8"]/ul//a/@href').getall()
-        print(movie_detail_urls)
-        pass
+        if movie_detail_urls:
+            for movie_detail_url in movie_detail_urls:
+                detail_url = response.urljoin(movie_detail_url)  # 拼接详情页url
+                # 如果要传递参数给detail_page_parse方法，在这里添加meta={"title", "123"}，然后在detail_page_parse使用response.meta.get("title", "")即可
+                yield scrapy.Request(detail_url, callback=self.detail_page_parse)  # 使用回调函数detail_page_parse解析response
+                break
+
+    def detail_page_parse(self, response):
+        title = response.css(".title_all h1 font::text").get()  # css选择器：.title_all选择所有有这个class的元素，然后筛选h1 font元素，然后提取text值
+        contents = response.css('#Zoom').get().replace('\u3000', '').replace('\xa0','').replace('\r\n','')  # 获取div标签内容，str类型   \xa0 是不间断空白符 \u3000 是全角的空白符
+        # 进行数据整理
+        print('====================================start',title)
+        if contents:
+            content_list = contents.split('<br>')    # 按<br>转换为list，正则就是一个灾难，果断放弃了，以后再转re吧
+            print(content_list)
+            for element in content_list:
+                # element.replace('\u3000', '').replace('\xa0','')  # \xa0 是不间断空白符 \u3000 是全角的空白符
+                # with open('./1.txt',"a+") as fa:
+                #     fa.write(str(element))
+                # print(element)
+                pass
+
+        print('====================================end',title)
+
